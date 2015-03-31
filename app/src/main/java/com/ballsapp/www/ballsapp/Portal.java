63b0cea2309e44d9b0fc88727237cd4b8bc4612d
Anthony Burzillo <burz@jhu.edu>
2015-03-31 14:54:44 -0400

    Add logo and handle back button

--- app/src/main/java/com/ballsapp/www/ballsapp/Portal.java
+++ app/src/main/java/com/ballsapp/www/ballsapp/Portal.java
@@ -9,11 +9,14 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.view.MotionEvent;
 import android.view.View;
-import 	android.webkit.WebView;
-import 	android.webkit.WebViewClient;
-import 	android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.webkit.WebSettings;
+import android.view.KeyEvent;
 
 public class Portal extends Activity {
+
+    private WebView myWebView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,10 +24,27 @@ public class Portal extends Activity {
 
         setContentView(R.layout.activity_portal);
 
-        WebView myWebView = (WebView) findViewById(R.id.webview);
+        myWebView = (WebView) findViewById(R.id.webview);
         myWebView.setWebViewClient(new WebViewClient());
         myWebView.loadUrl("http://www.ballsapp.com");
         WebSettings webSettings = myWebView.getSettings();
         webSettings.setJavaScriptEnabled(true);
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if(event.getAction() == KeyEvent.ACTION_DOWN) {
+            switch(keyCode)
+            {
+                case KeyEvent.KEYCODE_BACK:
+                    if(myWebView.canGoBack()) {
+                        myWebView.goBack();
+                    } else {
+                        finish();
+                    }
+                    return true;
+            }
+        }
+        return super.onKeyDown(keyCode, event);
     }
 }
