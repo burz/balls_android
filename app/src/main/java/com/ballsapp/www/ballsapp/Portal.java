b380877b9dd5c894147ca8a59c14ea4d6a9482f7
Anthony Burzillo <burz@jhu.edu>
2015-04-10 14:34:22 -0400

    Android Release 1.0

--- app/src/main/java/com/ballsapp/www/ballsapp/Portal.java
+++ app/src/main/java/com/ballsapp/www/ballsapp/Portal.java
@@ -11,8 +11,12 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.view.KeyEvent;
+import android.content.Intent;
+import android.net.Uri;
+import android.webkit.ValueCallback;
 
 public class Portal extends Activity {
 
@@ -27,24 +31,15 @@ public class Portal extends Activity {
         myWebView = (WebView) findViewById(R.id.webview);
         myWebView.setWebViewClient(new WebViewClient());
         myWebView.loadUrl("http://www.ballsapp.com");
+
         WebSettings webSettings = myWebView.getSettings();
         webSettings.setJavaScriptEnabled(true);
     }
 
     @Override
-    public boolean onKeyDown(int keyCode, KeyEvent event) {
-        if(event.getAction() == KeyEvent.ACTION_DOWN) {
-            switch(keyCode)
-            {
-                case KeyEvent.KEYCODE_BACK:
-                    if(myWebView.canGoBack()) {
-                        myWebView.goBack();
-                    } else {
-                        finish();
-                    }
-                    return true;
-            }
-        }
-        return super.onKeyDown(keyCode, event);
+    public void onResume() {
+        super.onResume();
+
+        myWebView.loadUrl("http://www.ballsapp.com");
     }
 }
